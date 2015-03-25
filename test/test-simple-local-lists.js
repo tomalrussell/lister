cecd1a9bce59037b9d338e12fc0df29f6b69a4d4
Tom Russell <tomalrussell@gmail.com>
2015-03-25 14:01:44 +0000

    specify data per-test

--- test/test-simple-local-lists.js
+++ test/test-simple-local-lists.js
@@ -1,93 +1,107 @@
-var post_one = {id:1,string:'test1'};
-var post_two = {id:2,string:'test2'};
-var post_two_alt = {id:2,string:'test2 changed'};
-
-var posts_in = [
-  post_one,
-  post_two,
-  {id:3,string:'test3'},
-];
-
-var new_post = {id: 7,string:'test7'};
-
-var posts_in_and_new = [
-  post_one,
-  post_two,
-  {id:3,string:'test3'},
-  new_post,
-];
-
-var more_posts = [
-  {id:4,string:'test4'},
-  {id:5,string:'test5'},
-  {id:6,string:'test6'},
-];
-
-var all_posts = [
-  post_one,
-  post_two,
-  {id:3,string:'test3'},
-  {id:4,string:'test4'},
-  {id:5,string:'test5'},
-  {id:6,string:'test6'},
-];
-
-var posts_in_except_two = [
-  post_one,
-  {id:3,string:'test3'},
-];
-
 describe("Storage", function() {
   it("sets and gets a list by name", function(){
-    lister.set("posts", posts_in);
+    lister.set("posts", [
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
 
     var posts = lister.get("posts");
-    expect(posts).toEqual(posts_in);
+    expect(posts).toEqual([
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
   });
 
   it("gets an item from a list by id", function() {
-    lister.set("posts", posts_in);
+    lister.set("posts", [
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
 
     var posts = lister.get("posts", 1);
-    expect(posts).toEqual(post_one);
+    expect(posts).toEqual({id:1,string:'test1'});
   });
 
   it("sets an item in a list by id", function() {
-    lister.set("posts", posts_in);
-    lister.set("posts", 2, post_two_alt);
+    lister.set("posts", [
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
+    lister.set("posts", 2, {id:2,string:'test2 changed'});
 
     var posts = lister.get("posts", 2);
-    expect(posts).toEqual(post_two_alt);
+    expect(posts).toEqual({id:2,string:'test2 changed'});
   });
 
   it("adds multiple items to a list", function(){
-    lister.set("posts", posts_in);
-    lister.add("posts", more_posts);
+    lister.set("posts", [
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
+    lister.add("posts", [
+      {id:4,string:'test4'},
+      {id:5,string:'test5'},
+      {id:6,string:'test6'},
+    ]);
 
     var posts = lister.get("posts");
-    expect(posts).toEqual(all_posts);
+    expect(posts).toEqual([
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+      {id:4,string:'test4'},
+      {id:5,string:'test5'},
+      {id:6,string:'test6'},
+    ]);
   });
 
   it("adds a single item to a list", function(){
-    lister.set("posts", posts_in);
-    lister.add("posts", new_post);
+    lister.set("posts", [
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
+    lister.add("posts", {id:7,string:'test7'});
 
     var posts = lister.get("posts");
-    expect(posts).toEqual(posts_in_and_new);
+    expect(posts).toEqual([
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+      {id:7,string:'test7'},
+    ]);
   });
 
   it("removes a list entirely", function(){
-    lister.set("posts",posts_in);
+    lister.set("posts",[
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
     lister.remove("posts");
+
     var posts = lister.get("posts");
     expect(posts).toBe(null);
   });
 
   it("removes a single item from a list", function(){
-    lister.set("posts",posts_in);
+    lister.set("posts",[
+      {id:1,string:'test1'},
+      {id:2,string:'test2'},
+      {id:3,string:'test3'},
+    ]);
     lister.remove("posts",2);
+
     var posts = lister.get("posts");
-    expect(posts).toEqual(posts_in_except_two);
+    expect(posts).toEqual([
+      {id:1,string:'test1'},
+      {id:3,string:'test3'},
+    ]);
   });
 
 });
